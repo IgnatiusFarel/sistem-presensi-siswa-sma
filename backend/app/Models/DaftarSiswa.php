ec81b632df324199ad52ed4f7f796bf67f810159
IgnatiusFarel <ignatiusloyolafarel17@gmail.com>
2025-05-24 22:52:20 +0700

    [UPDATE] [BACKEND] - Models - DaftarSiswa

--- backend/app/Models/DaftarSiswa.php
+++ backend/app/Models/DaftarSiswa.php
@@ -10,7 +10,10 @@ class DaftarSiswa extends Model
     use HasFactory;
 
     protected $table = 'daftar_siswa';
+    protected $primaryKey = 'daftar_siswa_id';
 
+    public $incrementing = false;
+    protected $keyType = 'string';
     protected $fillable = [
         'user_id',
         'nama',
@@ -22,18 +25,30 @@ class DaftarSiswa extends Model
         'nomor_handphone',
         'tempat_tanggal_lahir',
         'alamat_rumah',
-        'kelas',
-        'nomor_absen',        
+        'daftar_kelas_id',
+        'nama_kelas',
+        'nomor_absen',
         'tanggal_bergabung'
+    ];
+
+    protected $casts = [
+        'tanggal_bergabung' => 'date',
+        'jenis_kelamin' => 'string',
+        'agama' => 'string',
     ];
 
     public function user()
     {
-        return $this->belongsTo(User::class, 'user_id');
+        return $this->belongsTo(User::class, 'user_id', 'user_id');
+    }
+
+    public function kelas()
+    {
+        return $this->belongsTo(DaftarKelas::class, 'daftar_kelas_id', 'daftar_kelas_id');
     }
 
     public function presensiSiswa()
     {
-        return $this->hasMany(PresensiSiswa::class, 'daftar_siswa_id');
+        return $this->hasMany(PresensiSiswa::class, 'daftar_siswa_id', 'daftar_siswa_id');
     }
 }
