2b559f3ba660684679137881a2981faab64dffaf
IgnatiusFarel <ignatiusloyolafarel17@gmail.com>
2025-05-24 22:55:06 +0700

    [UPDATE] [BACKEND] - Migrations - CreateUsersTable

--- backend/database/migrations/2014_10_12_000000_create_users_table.php
+++ backend/database/migrations/2014_10_12_000000_create_users_table.php
@@ -9,18 +9,25 @@ class CreateUsersTable extends Migration
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id('user_id');                                  // user_id
+            $table->uuid('user_id')->primary();
             $table->string('name');
             $table->string('email')->unique();
-            $table->string('password');                        
-            $table->enum('role', ['superadmin','siswa'])
-                  ->default('siswa');                       
-            $table->unsignedBigInteger('created_by')->nullable();
-            $table->foreign('created_by')
-                    ->references('user_id')       // Referensi ke user_id bukan id
-                    ->on('users')
-                    ->onDelete('set null');
-            
+            $table->string('password');
+            $table->enum('role', [
+                'superadmin',
+                'kepala_sekolah',
+                'wakil_kepala_sekolah',
+                'guru',
+                'kepala_laboratorium',
+                'pustakawan',
+                'operator_sekolah',
+                'staf_tu',
+                'satpam',
+                'petugas_kebersihan',
+                'siswa'
+            ])->default('siswa');
+            $table->uuid('created_by')->nullable();
+            $table->foreign('created_by')->references('user_id')->on('users')->onDelete('set null');
             $table->rememberToken();
             $table->timestamps();
         });
