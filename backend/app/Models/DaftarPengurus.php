da317a0688fa024aa5d06fcdfecc19efcce3eae7
IgnatiusFarel <ignatiusloyolafarel17@gmail.com>
2025-05-24 22:51:49 +0700

    [UPDATE] [BACKEND] - Models - DaftarPengurus

--- backend/app/Models/DaftarPengurus.php
+++ backend/app/Models/DaftarPengurus.php
@@ -10,27 +10,94 @@ class DaftarPengurus extends Model
     use HasFactory;
 
     protected $table = 'daftar_pengurus';
-    
+    protected $primaryKey = 'daftar_pengurus_id';
+    public $incrementing = false;
+    protected $keyType = 'string';
+
+    public const JABATAN_ADMIN = 'Administrator';
+    public const JABATAN_KEPSEK = 'Kepala Sekolah';
+    public const JABATAN_WAKASEK = 'Wakil Kepala Sekolah';
+    public const JABATAN_GURU = 'Guru';
+    public const JABATAN_KALAB = 'Kepala Laboratorium';
+    public const JABATAN_PUSTAKAWAN = 'Pustakawan';
+    public const JABATAN_OPERATOR = 'Operator Sekolah';
+    public const JABATAN_TU = 'Staf TU';
+    public const JABATAN_SATPAM = 'Satpam';
+    public const JABATAN_KEBERSIHAN = 'Petugas Kebersihan';
+
+    public static function getAllJabatan()
+    {
+        return [
+            self::JABATAN_ADMIN,
+            self::JABATAN_KEPSEK,
+            self::JABATAN_WAKASEK,
+            self::JABATAN_GURU,
+            self::JABATAN_KALAB,
+            self::JABATAN_PUSTAKAWAN,
+            self::JABATAN_OPERATOR,
+            self::JABATAN_TU,
+            self::JABATAN_SATPAM,
+            self::JABATAN_KEBERSIHAN
+        ];
+    }
+
+    public static function getAllStatus()
+    {
+        return [
+            self::STATUS_PNS,
+            self::STATUS_HONORER,
+            self::STATUS_GTY,
+            self::STATUS_PTY,
+            self::STATUS_KONTRAK,
+            self::STATUS_MAGANG,
+            self::STATUS_PPPK,
+            self::STATUS_OUTSOURCING
+        ];
+    }
+
+    public const STATUS_PNS = 'PNS';
+    public const STATUS_HONORER = 'Honorer';
+    public const STATUS_GTY = 'GTY';
+    public const STATUS_PTY = 'PTY';
+    public const STATUS_KONTRAK = 'Kontrak';
+    public const STATUS_MAGANG = 'Magang';
+    public const STATUS_PPPK = 'PPPK';
+    public const STATUS_OUTSOURCING = 'Outsourcing';
+
     protected $fillable = [
         'user_id',
         'nama',
-        'nip',
         'jenis_kelamin',
         'agama',
-        'tempat_tanggal_lahir',
-        'alamat_rumah',
+        'nip',
         'email',
         'nomor_handphone',
-        'jabatan', // 'administrator' untuk superadmin, 'guru', 'staff', dll
-        'bidang_keahlian',        
+        'tempat_tanggal_lahir',
+        'alamat_rumah',
+        'jabatan',
+        'bidang_keahlian',
         'pengurus',
         'akses_kelas',
         'status_kepegawaian',
-        'tanggal_bergabung',        
+        'tanggal_bergabung',
     ];
 
+    protected $casts = [
+        'tanggal_bergabung' => 'date',
+        'jenis_kelamin' => 'string',
+        'agama' => 'string',
+        'jabatan' => 'string',
+        'status_kepegawaian' => 'string',
+    ];
     public function user()
     {
-        return $this->belongsTo(User::class, 'user_id');
+        return $this->belongsTo(User::class, 'user_id', 'user_id');
     }
+
+    public function waliKelas()
+    {
+        return $this->hasMany(DaftarKelas::class, 'wali_kelas', 'daftar_pengurus_id');
+    }
+
+
 }
