02bd5c013dc7b3647dd03b15fb39ee7f04c4a996
IgnatiusFarel <ignatiusloyolafarel17@gmail.com>
2025-05-24 22:59:51 +0700

    [ADD] [BACKEND] - Migrations - CreateDaftarSiswaTable

---
+++ backend/database/migrations/2025_05_18_150257_create_daftar_siswa_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDaftarSiswaTable extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('daftar_siswa', function (Blueprint $table) {
+            $table->uuid('daftar_siswa_id')->primary();
+            $table->uuid('user_id');
+            $table->foreign('user_id')->references('user_id')->on('users')->onDelete('cascade');
+            $table->string('nama');
+            $table->string('nis')->unique();
+            $table->string('nisn')->unique();
+            $table->enum('jenis_kelamin', ['Laki-laki', 'Perempuan']); 
+            $table->string('tempat_tanggal_lahir'); 
+            $table->enum('agama', ['Islam', 'Kristen', 'Katolik', 'Hindu', 'Buddha', 'Konghucu']);
+            $table->text('alamat');
+            $table->string('nomor_handphone');
+            $table->string('email')->unique();
+            $table->uuid('daftar_kelas_id');
+            $table->foreign('daftar_kelas_id')->references('daftar_kelas_id')->on('daftar_kelas')->onDelete('cascade');
+            $table->string('nama_kelas');
+            $table->integer('nomor_absen');
+            $table->date('tanggal_bergabung');            
+            $table->timestamps();
+        });
+    }
+   
+    public function down(): void
+    {
+        Schema::dropIfExists('daftar_siswa');
+    }
+};
