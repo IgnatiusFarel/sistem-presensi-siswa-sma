7b545a8fdeb484a90b1f2bae58016b60d2ab938e
IgnatiusFarel <ignatiusloyolafarel17@gmail.com>
2025-06-07 22:03:21 +0700

    [UPDATE] [FRONTEND] - App

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,5 +1,12 @@
 <script setup>
 import { NConfigProvider, NMessageProvider } from 'naive-ui'
+import { useAuthStore } from '@/stores/Auth'
+
+const authStore = useAuthStore()
+
+if (!authStore.isAuthenticated) {
+  authStore.logout()
+}
 </script>
 
 <template>
