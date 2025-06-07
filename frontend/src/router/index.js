81b169ac0b715e7ef373f8ef898e2b619fdd5aeb
IgnatiusFarel <ignatiusloyolafarel17@gmail.com>
2025-06-07 21:58:13 +0700

    [UPDATE] [FRONTEND] - Router - Index

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,4 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router';
+import { useAuthStore } from '@/stores/Auth.js' 
 import LoginPage from '@/pages/auth/LoginPage.vue';
 import InformationChangePage from '@/pages/auth/InformationChangePage.vue';
 import PresensiPage from '@/pages/presensi/PresensiPage.vue';
@@ -8,18 +9,63 @@ import DaftarKelasPage from '@/pages/daftar-kelas/DaftarKelasPage.vue';
 import PresensiSiswaPage from '@/pages/presensi-siswa/PresensiSiswaPage.vue';
 
 const routes = [
-  { path: '/masuk', component: LoginPage },
-  { path: '/perubahan-informasi', component: InformationChangePage },
-  { path: '/presensi', component: PresensiPage },
-  { path: '/daftar-siswa', component: DaftarSiswaPage },
-  { path: '/daftar-pengurus', component: DaftarPengurusPage },
-  { path: '/daftar-kelas', component: DaftarKelasPage },
-  { path: '/presensi-siswa', component: PresensiSiswaPage },
+  { path: '/masuk', component: LoginPage, meta: {public: true}},
+  { path: '/perubahan-informasi', component: InformationChangePage, meta: {public: true} },
+  { path: '/presensi', component: PresensiPage, meta: {requiresAuth: true, roles: ['superadmin']} },
+  { path: '/daftar-siswa', component: DaftarSiswaPage, meta: {requiresAuth: true, roles: ['superadmin']} },
+  { path: '/daftar-pengurus', component: DaftarPengurusPage, meta: {requiresAuth: true, roles: ['superadmin']} },
+  { path: '/daftar-kelas', component: DaftarKelasPage, meta: {requiresAuth: true, roles: ['superadmin']} },
+  { path: '/presensi-siswa', component: PresensiSiswaPage, meta: {requiresAuth: true, roles: ['siswa']} },
 ];
 
 const router = createRouter({
   history: createWebHistory(),
   routes,
 });
+
+router.beforeEach((to, from, next) => {
+  const authStore = useAuthStore()
+  const isAuth = authStore.isAuthenticated
+  const userRole = authStore.user?.role || null 
+
+  if (to.meta.public) {
+    if (isAuth) {
+      if (userRole === 'superadmin') {
+        return next ('/presensi')
+      }
+
+      if (userRole === 'siswa') {
+        return next ('/presensi-siswa')
+      }
+    }
+    return next()
+  }
+
+  if (to.meta.requiresAuth) { 
+    if (!isAuth) {
+      return next('/masuk')
+    }
+
+    const allowedRoles = to.meta.roles || []
+    if (allowedRoles.length > 0 && !allowedRoles.includes(userRole)) {
+      if (userRole === 'siswa') {
+        return next('/presensi-siswa')
+      }
+
+      if (userRole === 'superadmin') {
+        return next('/presensi')
+      }
+      return next('/masuk')
+    }
+    return next()
+  }
+
+  if (!to.meta.public && !to.meta.requiresAuth) {
+    if (!isAuth) {
+      return next('/masuk')
+    }
+  }
+ next()
+})
 
 export default router;
