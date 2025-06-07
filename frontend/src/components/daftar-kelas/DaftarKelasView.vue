2fdc4ba47d04ec1c2b819dc80bec072c65d9d89e
IgnatiusFarel <ignatiusloyolafarel17@gmail.com>
2025-06-07 22:03:08 +0700

    [UPDATE] [FRONTEND] - Components - Daftar Kelas -  Daftar Kelas View

--- frontend/src/components/daftar-kelas/DaftarKelasView.vue
+++ frontend/src/components/daftar-kelas/DaftarKelasView.vue
@@ -5,23 +5,25 @@
     @edit-data="showEditForm"
     @back-to-table="showView('Table')"
     :editData="editData"
+    :data="dataTable"
+    :loading="isLoading"
+    @delete-data="handleDelete"
+    @refresh="fetchData"
   />
 </template>
 
 <script setup>
-import { ref, shallowRef } from 'vue';
-import Table from './Table.vue';
-import TambahData from './TambahData.vue';
-import EditData from './EditData.vue';
+import { ref, shallowRef, onMounted } from "vue";
+import Table from "./Table.vue";
+import TambahData from "./TambahData.vue";
+import EditData from "./EditData.vue";
+import Api from "../../services/Api"
 
-const views = {
-  Table,
-  TambahData,
-  EditData,
-};
-
+const views = { Table, TambahData, EditData };
 const currentView = shallowRef(Table);
 const editData = ref(null);
+const isLoading = ref(false);
+const dataTable = ref([]);
 
 const showView = (viewName) => {
   currentView.value = views[viewName];
@@ -29,10 +31,26 @@ const showView = (viewName) => {
 
 const showEditForm = (data) => {
   editData.value = data;
-  showView('EditData');
+  showView("EditData");
 };
 
 const handleDelete = (keys) => {
-  console.log('Data yang akan dihapus:', keys);
+  console.log("Data yang akan dihapus:", keys);
 };
+
+const fetchData = async () => {
+  isLoading.value = true;
+  try {
+    const response = await Api.get("/daftar-kelas");
+    dataTable.value = response.data.data;
+  } catch (error) {
+    console.log(error);
+  } finally {
+    isLoading.value = false;
+  }
+};
+
+onMounted(() => {
+  fetchData();
+});
 </script>
