451d144c7db0094636275666d9bc3578c7f62d9f
IgnatiusFarel <ignatiusloyolafarel17@gmail.com>
2025-06-08 22:24:59 +0700

    [UPDATE] - Pages - Auth - Information Change Page

--- frontend/src/pages/auth/InformationChangePage.vue
+++ frontend/src/pages/auth/InformationChangePage.vue
@@ -3,12 +3,12 @@ import { useHead } from '@vueuse/head';
 import InformationChange from '@/components/auth/InformationChange.vue';
 
 useHead({
-  title: 'Halaman Daftar Kelas - Sistem Presensi Siswa SMA',
+  title: 'Halaman Perubahan Informasi - Sistem Presensi Siswa SMA',
   meta: [
     {
       name: 'description',
       content:
-        'Masuk sebagai admin untuk mengelola data kelas.',
+        'Masuk sebagai siswa untuk melakukan pelaporan perubahan informasi data siswa.',
     },
   ],
 });
