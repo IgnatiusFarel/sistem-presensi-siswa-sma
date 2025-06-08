90f05241c643718f45faaab9d2916de6a46dd952
IgnatiusFarel <ignatiusloyolafarel17@gmail.com>
2025-06-08 23:57:59 +0700

    [UPDATE] - Components - Daftar Siswa - View
    Add modal pop-up before delete data

--- frontend/src/components/daftar-siswa/DaftarSiswaView.vue
+++ frontend/src/components/daftar-siswa/DaftarSiswaView.vue
@@ -10,12 +10,24 @@
     @back-to-table="showView('Table')"
     @refresh="fetchData"
     @edit-data="showEditForm"
-    @delete-data="handleDelete"
+    @delete-data="confirmDelete"
+  />
+
+   <n-modal
+    v-model:show="showModal"
+    preset="dialog"
+    title="Konfirmasi Hapus"
+    content="Apakah Anda yakin ingin menghapus data siswa ini?"
+    positive-text="Ya, Hapus"
+    negative-text="Batal"
+    @positive-click="handleDelete"
+    @negative-click="() => showModal = false"
   />
 </template>
 
 <script setup>
 import { ref, shallowRef, onMounted } from 'vue';
+import { useMessage } from 'naive-ui';
 import Table from './Table.vue';
 import TambahData from './TambahData.vue';
 import EditData from './EditData.vue'
@@ -28,6 +40,10 @@ const loading = ref(false);
 const dataTable = ref([])
 const selectedRows = ref([]);
 
+const showModal = ref(false);
+const deleteTarget = ref(null); // Bisa single id atau array ids
+const message = useMessage();
+
 const showView = (viewName) => {
   currentView.value = views[viewName];
 };
@@ -37,29 +53,35 @@ const showEditForm = (data) => {
   showView('EditData');
 };
 
-const handleDelete = async (idOrIds) => {
+const confirmDelete = (idOrIds) => {
+  deleteTarget.value = idOrIds;
+  showModal.value = true;
+};
+
+const handleDelete = async () => {
+  showModal.value = false;
   loading.value = true;
+
   try {
-    if (Array.isArray(idOrIds)) {
-      // multiple delete
+    if (Array.isArray(deleteTarget.value)) {
+      // hapus banyak
       await Api.delete('/daftar-siswa', {
-        data: { ids: idOrIds },
+        data: { ids: deleteTarget.value },
       });
     } else {
-      // single delete
-      await Api.delete(`/daftar-siswa/${idOrIds}`);
+      // hapus satu
+      await Api.delete(`/daftar-siswa/${deleteTarget.value}`);
     }
-
-    // Refresh data
+    message.success('Data siswa berhasil dihapus!');
     await fetchData();
-     selectedRows.value = [];
-  } catch (err) {
-    console.error(err);
+    selectedRows.value = [];
+  } catch (error) {
+    message.error('Data siswa gagal dihapus!');
+    console.error(error);
   } finally {
     loading.value = false;
   }
 };
-
 
 const fetchData = async () => {
   loading.value =  true; 
