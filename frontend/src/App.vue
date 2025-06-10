ebee313c9d4752851b0f860f21901013aab676f0
IgnatiusFarel <ignatiusloyolafarel17@gmail.com>
2025-06-10 18:39:06 +0700

    [UPDATE] - App

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,16 +1,16 @@
 <script setup>
 import { NConfigProvider, NMessageProvider } from 'naive-ui'
 import { useAuthStore } from '@/stores/Auth'
+import { themeOverrides } from '@/theme'
 
 const authStore = useAuthStore()
-
 if (!authStore.isAuthenticated) {
   authStore.logout()
 }
 </script>
 
 <template>
-  <n-config-provider>
+  <n-config-provider :theme-overrides="themeOverrides">
     <n-message-provider>
     <router-view />
   </n-message-provider>
