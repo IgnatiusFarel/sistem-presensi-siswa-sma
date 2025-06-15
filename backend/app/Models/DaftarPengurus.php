fb6d1671444dbb99dfd3e09e304a889c74f47b1c
IgnatiusFarel <ignatiusloyolafarel17@gmail.com>
2025-06-15 23:03:21 +0700

    [UPDATE] - Models - Daftar Pengurus

--- backend/app/Models/DaftarPengurus.php
+++ backend/app/Models/DaftarPengurus.php
@@ -2,10 +2,10 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Str;
-use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class DaftarPengurus extends Model
 {
