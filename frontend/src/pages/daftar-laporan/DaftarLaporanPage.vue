b997d54258c27bfff77ad1047d41e583f5579c56
IgnatiusFarel <ignatiusloyolafarel17@gmail.com>
2025-06-17 22:16:33 +0700

    [ADD] - Pages - Daftar Laporan Page

---
+++ frontend/src/pages/daftar-laporan/DaftarLaporanPage.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { useHead } from '@vueuse/head';
+import DaftarLaporanView from '@/components/daftar-laporan/DaftarLaporanView.vue';
+
+useHead({
+  title: 'Halaman Daftar Laporan - Sistem Presensi Siswa SMA',
+  meta: [
+    {
+      name: 'description',
+      content:
+        'Masuk sebagai admin untuk mengelola data laporan.',
+    },
+  ],
+});
+</script>
+
+<template>
+  <DashboardLayout>
+    <DaftarLaporanView />
+  </DashboardLayout>
+</template>
