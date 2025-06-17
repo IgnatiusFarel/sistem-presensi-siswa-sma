9c8d84864c8bc1bd515aed72b625144bc4a8e323
IgnatiusFarel <ignatiusloyolafarel17@gmail.com>
2025-06-17 22:11:48 +0700

    [UPDATE] - Components - Dashboard Layout - Admin

--- frontend/src/components/layout/admin/DashboardLayout.vue
+++ frontend/src/components/layout/admin/DashboardLayout.vue
@@ -1,8 +1,7 @@
 <template>
   <div class="flex h-screen">
     <Sidebar />
-    <main class="flex-1 overflow-y-auto p-6">
-      <!-- konten dari tiap page akan masuk di sini -->
+    <main class="flex-1 overflow-y-auto p-6">      
       <slot />
     </main>
   </div>
