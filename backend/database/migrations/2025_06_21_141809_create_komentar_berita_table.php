c86ccc571f1eb130369891af645343b198474ec8
IgnatiusFarel <ignatiusloyolafarel17@gmail.com>
2025-06-21 23:29:54 +0700

    [ADD] - Migrations - Create Komentar Berita Table

---
+++ backend/database/migrations/2025_06_21_141809_create_komentar_berita_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateKomentarBeritaTable extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('komentar_berita', function (Blueprint $table) {
+            $table->uuid('komentar_berita_id')->primary();
+            $table->uuid('user_id');
+            $table->foreign('user_id')->references('user_id')->on('users')->onDelete('cascade');
+            $table->uuid('daftar_berita_id');
+            $table->foreign('daftar_berita_id')->references('daftar_berita_id')->on('daftar_berita')->onDelete('cascade');
+            $table->text('komentar');
+            
+            $table->timestamps();
+        });
+    }
+ 
+    public function down(): void
+    {
+        Schema::dropIfExists('komentar_berita');
+    }
+};
