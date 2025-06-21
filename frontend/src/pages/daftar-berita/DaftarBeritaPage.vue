77c5599461d3e57175c12d3e969f8807cae6a13c
IgnatiusFarel <ignatiusloyolafarel17@gmail.com>
2025-06-21 23:31:07 +0700

    [ADD] - Pages - Daftar Berita Page

---
+++ frontend/src/pages/daftar-berita/DaftarBeritaPage.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { useHead } from '@vueuse/head';
+import DaftarBeritaView from '@/components/daftar-berita/DaftarBeritaView.vue';
+useHead({
+  title: 'Halaman Daftar Berita - Sistem Presensi Siswa SMA',
+  meta: [
+    {
+      name: 'description',
+      content:
+        'Masuk sebagai admin untuk mengelola data berita.',
+    },
+  ],
+});
+</script>
+
+<template>
+  <DashboardLayout>
+    <DaftarBeritaView />
+  </DashboardLayout>
+</template>
