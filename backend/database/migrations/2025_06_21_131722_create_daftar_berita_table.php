b70273a998e741c2429d6c0f37d2c9e0b3caae36
IgnatiusFarel <ignatiusloyolafarel17@gmail.com>
2025-06-21 23:29:27 +0700

    [ADD] - Migrations - Create Daftar Berita Table

---
+++ backend/database/migrations/2025_06_21_131722_create_daftar_berita_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+Class CreateDaftarBeritaTable extends Migration
+{   
+    public function up(): void
+    {
+        Schema::create('daftar_berita', function (Blueprint $table) {
+            $table->uuid('daftar_berita_id')->primary();
+            $table->string('slug')->unique();
+            $table->string('judul');
+            $table->string('thumbnail')->nullable();
+            $table->enum('kategori', ['pengumuman', 'kegiatan', 'prestasi', 'informasi', 'agenda', 'lainnya'])->default('informasi');
+            $table->text('konten');
+            $table->uuid('user_id');
+            $table->foreign('user_id')->references('user_id')->on('users')->onDelete('cascade');            
+            $table->string('dibuat_oleh');
+            $table->timestamps();
+        });
+    }
+ 
+    public function down(): void
+    {
+        Schema::dropIfExists('daftar_berita');
+    }
+};
