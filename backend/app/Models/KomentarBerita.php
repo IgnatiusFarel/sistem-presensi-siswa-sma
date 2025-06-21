95b4b44ebfb9711e8364943ff31a822f7b9055b2
IgnatiusFarel <ignatiusloyolafarel17@gmail.com>
2025-06-21 23:29:06 +0700

    [ADD] - Models - Komentar Berita

---
+++ backend/app/Models/KomentarBerita.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
+
+class KomentarBerita extends Model
+{
+    use HasFactory;
+    protected $table = 'komentar_berita';
+    protected $primaryKey = 'komentar_berita_id';
+    public $incrementing = false;
+    protected $keyType = 'string';
+    protected $fillable = [
+        'komentar_berita_id',
+        'user_id',
+        'daftar_berita_id',
+        'komentar',
+    ];
+
+      protected static function booted()
+    {
+        static::creating(function ($model) {            
+            if (empty($model->komentar_berita_id)) {
+                $model->komentar_berita_id = (string) Str::uuid();
+            }
+        });
+    }
+
+    public function berita()
+    {
+        return $this->belongsTo(DaftarBerita::class, 'daftar_berita_id', 'daftar_berita_id');
+    }
+    
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id', 'user_id');
+    }
+}
