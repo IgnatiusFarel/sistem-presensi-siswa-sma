d398d6952a0f43d239abf1661896d731d606ce1c
IgnatiusFarel <ignatiusloyolafarel17@gmail.com>
2025-06-21 23:28:53 +0700

    [ADD] - Models - Daftar Berita

---
+++ backend/app/Models/DaftarBerita.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
+class DaftarBerita extends Model
+{
+    use HasFactory;
+    protected $table = 'daftar_berita';
+    protected $primaryKey = 'daftar_berita_id';
+    public $incrementing = false;
+    protected $keyType = 'string';
+    protected $fillable = [
+        'daftar_berita_id',
+        'slug',
+        'judul',
+        'thumbnail',
+        'kategori',
+        'konten',
+        'user_id',
+        'dibuat_oleh',
+    ];
+
+    protected static function booted()
+    {
+        static::creating(function ($model) {
+            if (empty($model->daftar_berita_id)) {
+                $model->daftar_berita_id = (string) Str::uuid();
+            }
+        });
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id', 'user_id');
+    }
+
+    public function komentar()
+    {
+        return $this->hasMany(KomentarBerita::class, 'daftar_berita_id', 'daftar_berita_id');
+    }
+}
