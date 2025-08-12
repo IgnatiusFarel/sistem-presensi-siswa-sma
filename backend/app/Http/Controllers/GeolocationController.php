506904ad64cf82556b70219647e69e9d613e2bb4
IgnatiusFarel <ignatiusloyolafarel17@gmail.com>
2025-08-12 23:11:39 +0700

    [UPDATE] - Controllers - Geo Location Controller

--- backend/app/Http/Controllers/GeolocationController.php
+++ backend/app/Http/Controllers/GeolocationController.php
@@ -2,32 +2,38 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Support\Facades\Http;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 
 class GeolocationController extends Controller
 {
     public function reverseGeocode(Request $request)
     {
-        $lat = $request->query('lat');
-        $lon = $request->query('lon');
+        try {
+            $lat = $request->query('lat');
+            $lon = $request->query('lon');
 
-        if (!$lat || !$lon) {
-            return response()->json(['error' => 'Invalid coordinates'], 400);
+            if (!$lat || !$lon) {
+                return response()->json(['error' => 'Invalid coordinates'], 400);
+            }
+
+            $response = Http::withHeaders([
+                'User-Agent' => 'SistemPresensiSiswaSMA/1.0 (ignatius@email.com)'
+            ])->get('https://nominatim.openstreetmap.org/reverse', [
+                        'format' => 'json',
+                        'lat' => $lat,
+                        'lon' => $lon,
+                    ]);
+
+            if ($response->successful()) {
+                return $response->json();
+            }
+
+            return response()->json(['error' => 'Failed to fetch location'], $response->status());
+
+        } catch (\Throwable $th) {
+            $statusCode = isset($response) ? $response->status() : 500;
+            return response()->json(['error' => 'Failed to fetch location'], $statusCode);
         }
-
-        $response = Http::withHeaders([
-            'User-Agent' => 'SistemPresensiSiswaSMA/1.0 (ignatius@email.com)'
-        ])->get('https://nominatim.openstreetmap.org/reverse', [
-                    'format' => 'json',
-                    'lat' => $lat,
-                    'lon' => $lon,
-                ]);
-
-        if ($response->successful()) {
-            return $response->json();
-        }
-
-        return response()->json(['error' => 'Failed to fetch location'], $response->status());
     }
 }
