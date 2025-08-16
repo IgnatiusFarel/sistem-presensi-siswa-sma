d3b689d05a35be208e550c9dd1904b99e0e13df5
IgnatiusFarel <ignatiusloyolafarel17@gmail.com>
2025-08-17 00:03:12 +0700

    [UPDATE] - Components - Presensi Siswa - Riwayat - Information Card

--- frontend/src/components/presensi-siswa/riwayat/InformationCard.vue
+++ frontend/src/components/presensi-siswa/riwayat/InformationCard.vue
@@ -1,11 +1,11 @@
 <template>
-  <div class="flex flex-row px-5 gap-2 mb-4">
+    <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 px-5 mb-4">
  <n-card
       v-for="item in stats"
       :key="item.title"
       :title="item.title"
       hoverable
-      class="!border-[#C1C2C5] !rounded-xl !max-w-[300px]"
+         class="!border-[#C1C2C5] !rounded-xl"
     >
       <p class="text-base font-bold">{{ item.count }} / {{ total }} Kegiatan Presensi</p>
     </n-card>
