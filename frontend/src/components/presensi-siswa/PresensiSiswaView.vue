6739467deb1d40834ee07daee528da7679da4073
IgnatiusFarel <ignatiusloyolafarel17@gmail.com>
2025-08-17 00:01:05 +0700

    [UPDATE] - Components - Presensi Siswa - View

--- frontend/src/components/presensi-siswa/PresensiSiswaView.vue
+++ frontend/src/components/presensi-siswa/PresensiSiswaView.vue
@@ -2,9 +2,8 @@
   <div class="px-5">
     <n-tabs type="line" animated v-model:value="activeTab" class="mb-4">
       <n-tab-pane name="presensi" tab="Kegiatan Presensi Hari Ini">
-        <h1 class="text-2xl text-[#232323] font-bold">
-          Kegiatan Presensi Hari Ini
-        </h1>
+        <h1 class="text-2xl font-bold transition-colors duration-300"
+      :class="themeStore.isDark ? 'bg-neutral-900' : 'bg-gray-50'">Kegiatan Presensi Hari Ini</h1>
 
         <div v-if="presensiAktif" class="flex items-center gap-2 mt-2">
           <p class="text-base text-[#232323] font-semibold">Dibuka Pukul</p>
@@ -23,16 +22,18 @@
         <div v-else class="flex items-center gap-2 text-base mt-2">
           <span class="text-red-600">Belum Ada Kegiatan Presensi</span>
           <span class="text-gray-400">|</span>
-          <span class="text-[#232323] font-semibold">{{ currentDate }}</span>
+         <p class="text-base font-semibold transition-colors duration-300" :class="themeStore.isDark ? 'bg-neutral-900' : 'bg-gray-50'">{{ currentDate }}</p>
         </div>
       </n-tab-pane>
       <n-tab-pane name="riwayat" tab="Riwayat Kegiatan Presensi Kamu">
-        <h1 class="text-2xl text-[#232323] font-bold">
+        <h1 class="text-2xl font-bold transition-colors duration-300"
+      :class="themeStore.isDark ? 'bg-neutral-900' : 'bg-gray-50'">
           Riwayat Kegiatan Presensi
         </h1>
       </n-tab-pane>
       <n-tab-pane name="berita" tab="Berita Sekolah">
-        <h1 class="text-2xl text-[#232323] font-bold">Berita Terbaru</h1>
+      <h1 class="text-2xl font-bold transition-colors duration-300"
+      :class="themeStore.isDark ? 'bg-neutral-900' : 'bg-gray-50'">Berita Terbaru</h1>
       </n-tab-pane>
     </n-tabs>
   </div>
@@ -47,12 +48,14 @@ import Presensi from "./presensi/PresensiSiswa.vue";
 import Riwayat from "./riwayat/RiwayatPresensiSiswa.vue";
 import Berita from "./berita/BeritaView.vue";
 import Api from "@/services/Api.js";
+import { useThemeStore } from "@/stores/ThemeMode";
 
 const activeTab = ref("presensi");
 const presensiAktif = ref(null);
 const currentTime = ref("");
 const currentDate = ref("");
 const loading = ref(false);
+const themeStore = useThemeStore()
 
 const updateDateTime = () => {
   const now = new Date();
